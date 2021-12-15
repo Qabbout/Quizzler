37cef1c67c83fffa3c6202df3c12725cbb4b8329
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2021-12-15 11:12:18 +0200

    added basic answering functionality

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let quiz = [
+        ["4 + 2 = 6", "True"],
+        ["5 - 3 > 1", "True"],
+        ["3 + 8 = 10", "False"],
+    ]
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.titleLabel!.text!
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
+        }
+        else {
+            print("Wrong")
+        }
+        
+        questionNumber += 1
+        updateUI()
+    }
+    
+    func updateUI(){
+        questionLabel.text = quiz[questionNumber][0]
+    }
+    
 }
 
